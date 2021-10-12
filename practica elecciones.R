c402db3a22a234b13b73bda084698e9d00d277f0
LeanColisko <colisko_leandro@live.com.ar>
2021-10-12 20:28:29 -0300

    datos de circuitos electorales y script

--- practica elecciones.R
+++ practica elecciones.R
@@ -14,10 +14,10 @@ str(datos)
 # Resumen de las variables
 summary(datos)
 
-# Primeros datos
+# Primeras 6 lineas de datos
 head(datos)
 
-# Últimos datos
+# Últimas 6 lineas de datos
 tail(datos)
 
 
@@ -42,8 +42,8 @@ datos_clean <- datos %>%
 
 
 # 4 - Resultados por agrupación -------------------
-# Con los datos limpios vamos a ver la suma de votos de cada agrupación
 
+# Con los datos limpios vamos a ver la suma de votos de cada agrupación
 datos_clean %>% 
   group_by(NOMBRE_AGRUPACION) %>% 
   summarise(PASO_x_AGRUP = sum(PASO),
@@ -52,7 +52,7 @@ datos_clean %>%
          `% GRAL` = GRAL_x_AGRUP / sum(GRAL_x_AGRUP) * 100)
 
 
-# Vamos a ver los resultados para Avellaneda
+# Resultados para Avellaneda
 datos_clean %>% 
   filter(SECCION == 'Avellaneda') %>% 
   group_by(NOMBRE_AGRUPACION) %>% 
@@ -61,11 +61,105 @@ datos_clean %>%
   mutate(`% PASO` = PASO_x_AGRUP / sum(PASO_x_AGRUP) * 100,
          `% GRAL` = GRAL_x_AGRUP / sum(GRAL_x_AGRUP) * 100)
 
-# Datos para La Matanza
-datos_clean %>% 
-  filter(SECCION == 'La Matanza') %>% 
-  group_by(NOMBRE_AGRUPACION) %>% 
+
+
+# Datos para todas las secciones
+resultados <- datos_clean %>% 
+  group_by(SECCION, NOMBRE_AGRUPACION) %>% 
   summarise(PASO_x_AGRUP = sum(PASO),
             GRAL_x_AGRUP = sum(GRAL)) %>% 
   mutate(`% PASO` = PASO_x_AGRUP / sum(PASO_x_AGRUP) * 100,
-         `% GRAL` = GRAL_x_AGRUP / sum(GRAL_x_AGRUP) * 100)
+         `% GRAL` = GRAL_x_AGRUP / sum(GRAL_x_AGRUP) * 100) %>% 
+  arrange(SECCION, desc(`% GRAL`)) %>% 
+  mutate(Puestos_GRAL = row_number()) %>% 
+  arrange(SECCION, desc(`% PASO`)) %>% 
+  mutate(Puestos_PASO = row_number())
+
+
+# Mostramos los resultados de quiene salieron primeros en la GRAL 
+resultados %>%
+  filter(Puestos_GRAL==1)
+
+
+
+# 5 - Transposición de datos ----------------
+
+# Queremos construir una tabla con los datos 
+# A nivel de circuito. Cada fila representa un circuito y los circuitos 
+# No están duplicados
+
+# Esto lo podemos hacer pivoteando con las funciones de tidyr
+library(tidyr)
+
+datos_circuito <- datos_clean %>% 
+  pivot_wider(id_cols = c(SECCION, Circuito), 
+              names_from = NOMBRE_AGRUPACION,
+              names_sep = " ; ",
+              values_from = c(PASO, GRAL))
+
+ganadores_gral <- datos_clean %>% 
+  filter(GRAL != 0) %>% 
+  group_by(SECCION, Circuito) %>% 
+  arrange(Circuito, desc(GRAL)) %>% 
+  mutate(POSICION_GRALES = row_number()) %>%
+  ungroup() %>% 
+  filter(POSICION_GRALES == 1) %>% 
+  select(SECCION, Circuito, NOMBRE_AGRUPACION) %>% 
+  rename(GANADOR_GENERAL = NOMBRE_AGRUPACION)
+
+ganadores_paso <- datos_clean %>% 
+  filter(PASO != 0) %>% 
+  group_by(SECCION, Circuito) %>% 
+  arrange(Circuito, desc(PASO)) %>% 
+  mutate(POSICION_PASO = row_number()) %>%
+  ungroup() %>% 
+  filter(POSICION_PASO == 1) %>% 
+  select(SECCION, Circuito, NOMBRE_AGRUPACION) %>% 
+  rename(GANADOR_PASO = NOMBRE_AGRUPACION)
+
+
+ganadores <- ganadores_gral %>% 
+  left_join(ganadores_paso)
+
+
+
+# 6 - Lectura de datos geográficos ---------------
+library(sf)
+
+# Datos del circuito electoral
+circuitos <- sf::read_sf("datos/circuitos-electorales/circuitos-electorales.shp")
+
+
+library(ggplot2)
+ggplot(data = circuitos) +
+  geom_sf()
+
+
+
+# 7 - Unión de datos geográficos y electorales ----------
+circuitos <- circuitos %>% 
+  left_join(ganadores, by = c("circuito"="Circuito")) %>% 
+  filter(!is.na(SECCION))
+
+## Graficamos los datos de los ganadores de la eleccion PASO
+ggplot(data = circuitos, aes(fill = GANADOR_PASO)) +
+  geom_sf() +
+  scale_fill_manual(values = c("#18C2EC", "#FFFF33")) +
+  labs(title = "Elección PASO 2019", 
+       subtitle = "Categoría a Presidente. Ganador por circuito electoral.",
+       caption = "Tercera sección electoral") +
+  theme(legend.position = c(0.1, 0.1))
+
+
+## Graficamos los datos de los ganadores de la eleccion GRAL
+ggplot(data = circuitos, aes(fill = GANADOR_GENERAL)) +
+  geom_sf() +
+  scale_fill_manual(values = c("#18C2EC", "#FFFF33")) +
+  labs(title = "Elección GENERAL 2019", 
+       subtitle = "Categoría a Presidente. Ganador por circuito electoral.",
+       caption = "Tercera sección electoral") +
+  theme(legend.position = c(0.1, 0.1))
+
+
+
+
